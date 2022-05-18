42afacc27587bc5559e2891ef662ee9dfccb8ff2
Nathaniel Cruel <nccruel@up.edu.ph>
2022-05-18 23:40:31 +0800

    Initialize major components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Budget from './components/Budget';
+import Balance from './components/Balance';
+import TotalExpenses from './components/TotalExpenses';
 
 const App = () => {
-	return <p>Hello React!</p>;
+	return (
+			<div className='container'>
+				<h1 className='mt-3'>My Budget Planner</h1>
+				<div className='row mt-3'>
+					<div className='col-sm'>
+						<Budget />
+					</div>
+					<div className='col-sm'>
+						<Balance />
+					</div>
+					<div className='col-sm'>
+						<TotalExpenses />
+					</div>
+				</div>
+			</div>
+	);
 };
 
 export default App;
---
+++ src/components/Balance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Balance = () => {
+	return (
+		<div className='alert alert-success'>
+			<span>Balance: ₱1000</span>
+		</div>
+	);
+};
+
+export default Balance;
